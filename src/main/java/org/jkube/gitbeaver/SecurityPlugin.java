9da4531339e844d79e3a8c6c89a94149d7684a16
Jan Kneissler <jan.kneissler@breuninger.de>
2022-10-27 20:03:04 +0200

    Move plugin to parent package

--- src/main/java/org/jkube/gitbeaver/SecurityPlugin.java
+++ src/main/java/org/jkube/gitbeaver/SecurityPlugin.java
@@ -1,6 +1,7 @@
-package org.jkube.gitbeaver.security;
+package org.jkube.gitbeaver;
 
 import org.jkube.gitbeaver.plugin.SimplePlugin;
+import org.jkube.gitbeaver.security.SecurityManagement;
 import org.jkube.gitbeaver.security.commands.CreateKeyPairCommand;
 import org.jkube.gitbeaver.security.commands.EncryptCommand;
 import org.jkube.gitbeaver.security.commands.MasterKeyPresentCommand;
@@ -20,7 +21,7 @@ import org.jkube.logging.Log;
 
         @Override
         public void init() {
-            Log.log("Security master key present: "+SecurityManagement.masterKeyWasFound());
+            Log.log("Security master key present: "+ SecurityManagement.masterKeyWasFound());
         }
 
     }
